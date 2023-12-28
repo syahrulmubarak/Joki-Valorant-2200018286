423e7bd71f2c64cc240d3a15626ed77179b37ff5
syahhrul <128670714+syahhrul@users.noreply.github.com>
2023-12-28 19:32:43 +0700

    fisrt commit

---
+++ src/Invoice.java
@@ -0,0 +1,12 @@
+public class Invoice implements Home{
+
+
+
+    public void tampilkan(){
+        System.out.println("----------------------------");
+        System.out.println("=\tINVOICE\t=");
+        System.out.println("----------------------------");
+    }
+
+
+}
